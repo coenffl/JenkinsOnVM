5d6134ff216b76cf876bf0520e51c56fc660a83b
coenffl <maroon03@naver.com>
2021-07-15 13:18:16 +0900

    6th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I will really miss u nunu');
+    res.send('I will really miss u nunu!');
 });
 
 app.get('/start', function(req, res){
