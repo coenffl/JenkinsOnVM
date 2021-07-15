81209fadd08e7558f1fab550e5f3cc7dcbb2e540
coenffl <maroon03@naver.com>
2021-07-15 16:27:00 +0900

    8th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('go5rae come here I need u');
+    res.send('cofl come here, I need u');
 });
 
 app.get('/start', function(req, res){
