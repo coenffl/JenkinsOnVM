eec8e192fab022e9944fef7544245b22e771dbd0
coenffl <maroon03@naver.com>
2021-07-15 13:02:56 +0900

    3rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I really like u, coenffl!');
+    res.send('I really like u, kiki');
 });
 
 app.get('/start', function(req, res){
