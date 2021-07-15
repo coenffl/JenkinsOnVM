587a30222d8b6e4a0e1c14543565166eeaa26fa0
coenffl <maroon03@naver.com>
2021-07-15 16:20:03 +0900

    6th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('Love ya, kiki!!');
+    res.send('go5rae come here');
 });
 
 app.get('/start', function(req, res){
